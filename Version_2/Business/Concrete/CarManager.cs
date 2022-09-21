045f7bc9dffe98380d40db0ac8633f21b48ae18c
fatihbereketinan <fatihbereketinan@gmail.com>
2022-09-21 22:22:07 +0300

    Version_2

---
+++ Version_2/Business/Concrete/CarManager.cs
@@ -0,0 +1,61 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using Entities.DTOs;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class CarManager : ICarService
+    {
+        ICarDal _carDal;
+        public CarManager(ICarDal carDal)
+        {
+            _carDal = carDal;
+        }
+        public void Add(Car car)
+        {
+            if (car.Model.Length > 2 && car.DailyPrice > 0)
+            {
+                _carDal.Add(car);
+            }
+            else
+            {
+                throw new InvalidDataException();
+            }
+        }
+
+        public void Delete(Car car)
+        {
+            _carDal.Delete(car);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _carDal.GetAll();
+        }
+
+        public List<CarDetailDto> GetCarDetails()
+        {
+            return _carDal.GetCarDetails();
+        }
+
+        public List<Car> GetCarsByBrandId(int brandId)
+        {
+            return _carDal.GetAll(c => c.BrandId == brandId);
+        }
+
+        public List<Car> GetCarsByColorId(int colorId)
+        {
+            return _carDal.GetAll(c => c.ColorId == colorId);
+        }
+
+        public void Update(Car car)
+        {
+            _carDal.Update(car);
+        }
+    }
+}
---
+++ Version_2/ConsoleUI/Program.cs
@@ -0,0 +1,46 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using System;
+
+namespace ConsoleUI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DtoCar();
+            //DeleteCar();
+            //UpdateCar();
+            //AddCar();
+
+        }
+
+        private static void AddCar()
+        {
+            CarManager carManager = new CarManager(new EfCarDal());
+            carManager.Add(new Car() { ColorId = 5, BrandId = 2, Model = "AudiA8", DailyPrice = 1000, ModelYear = 2022, Description = "Premium" });
+        }
+
+        private static void UpdateCar()
+        {
+            CarManager carManager = new CarManager(new EfCarDal());
+            carManager.Update(new Car() { Id = 1, ColorId = 1, BrandId = 1, Model = "Corolla", DailyPrice = 500, ModelYear = 2022, Description = "Premium" });
+        }
+
+        private static void DeleteCar()
+        {
+            CarManager carManager = new CarManager(new EfCarDal());
+            carManager.Delete(new Car() { Id = 1002 });
+        }
+
+        private static void DtoCar()
+        {
+            CarManager carManager = new CarManager(new EfCarDal());
+            foreach (var car in carManager.GetCarDetails())
+            {
+                Console.WriteLine(car.BrandName + " / " + car.CarName + " / " + car.ColorName + " / " + car.DailyPrice);
+            }
+        }
+    }
+}
